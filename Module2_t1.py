a880e4e4b5957e97a081750ce0622ee5486c3d9f
Vishal-Pal002 <pal886655@gmail.com>
2025-09-30 18:08:57 +0530

    Add files via upload

---
+++ Module2_t1.py
@@ -0,0 +1,14 @@
+x=int(input("Enter Your Value:"))
+y=int(input("Enter Your Value:"))
+print()
+
+a=x+y
+b=x-y
+c=x*y
+d=x/y
+
+print("addition:",a)
+print("subtraction:",b)
+print("multiplication:",c)
+print("division:",d)
+
---
+++ Module2_t2.py
@@ -0,0 +1,11 @@
+a=input("Enter Your First Name: ")
+b=input("Enter Your Last Name: ")
+print()
+
+c= a + " " + b
+
+print(f"Hello, {c}! Welcome to Python Program")
+
+
+
+
