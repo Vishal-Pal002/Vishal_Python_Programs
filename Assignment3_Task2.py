b1aec65c2c700179de3c7f27f553b2631fc71607
Vishal-Pal002 <pal886655@gmail.com>
2025-10-01 11:54:22 +0530

    Add files via upload

---
+++ Assignment3_Task2.py
@@ -0,0 +1,10 @@
+import math
+x =int(input("Enter a number: "))
+
+square_root = math.sqrt(x)
+log = math.log(x)
+sine = math.sin(x)
+
+print(f"Square root : {square_root}")
+print(f"logarithm : {log}")
+print(f"Sine : {sine}")
